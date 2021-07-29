f73598bea58d21acffe5e18fda92ced8914e2b89
꧁ Romenildo꧂ <53869976+Romenildo@users.noreply.github.com>
2021-07-29 08:51:58 -0300

    Create Bubble_Sorte.java

---
+++ JAVA/Bubble_Sorte.java
@@ -0,0 +1,15 @@
+public class Bolha {
+ 
+    public void bubbleSort(int v[]) {
+ 
+        for (int i = v.length; i >= 1; i--) {
+            for (int j = 1; j < i; j++) {
+                if (v[j - 1] > v[j]) {
+                    int aux = v[j];
+                    v[j] = v[j - 1];
+                    v[j - 1] = aux;
+                }
+            }
+        }
+    }
+}
